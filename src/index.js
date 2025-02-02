c8e79285ef4d9f56d3f1bac564600249e4d29c69
KrystynaOrda <ordakrystyna@gmail.com>
2025-02-02 09:11:33 +0000

    test lit packages

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { ethers, Wallet } from "ethers";
+import { LitContracts } from "@lit-protocol/contracts-sdk";
+import { LitNodeClient } from "@lit-protocol/lit-node-client";
+
+// init lit and make it a service
